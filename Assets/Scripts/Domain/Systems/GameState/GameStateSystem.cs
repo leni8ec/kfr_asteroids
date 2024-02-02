e3a3afc16c7692b9590b716c9df412f5d1ac8b3b
leni8ec <leni8ec@gmail.com>
2024-02-02 08:05:14 +0300

    Score system

--- Assets/Scripts/Domain/Systems/GameState/GameStateSystem.cs
+++ Assets/Scripts/Domain/Systems/GameState/GameStateSystem.cs
@@ -1,9 +1,12 @@
-﻿using Core.Interface.Objects;
+﻿using System;
+using Core.Interface.Objects;
 using Domain.Systems.Collision;
-using UnityEngine;
 
 namespace Domain.Systems.GameState {
     public class GameStateSystem {
+        public static event Action NewGameEvent;
+        public static event Action GameOverEvent;
+
         public GameStateSystem() {
             CollisionSystem.PlayerHit += PlayerHitHandler;
         }
@@ -13,6 +16,7 @@ namespace Domain.Systems.GameState {
         }
 
         private void GameOver() {
+            GameOverEvent?.Invoke();
             // todo
             // Debug.LogWarning("Game Over!");
         }
--- Assets/Scripts/Domain/Systems/Gameplay/ScoreSystem.cs
+++ Assets/Scripts/Domain/Systems/Gameplay/ScoreSystem.cs
@@ -1,7 +1,26 @@
-﻿namespace Domain.Systems.Gameplay {
-    public class ScoreSystem {
+﻿using Core.Data;
+using Core.Interface.Objects;
+using Domain.Systems.Collision;
+using Domain.Systems.GameState;
 
-        // Not required
+namespace Domain.Systems.Gameplay {
+    public class ScoreSystem {
+        public ScoreData Data { get; }
+
+        public ScoreSystem(ScoreData data) {
+            Data = data;
+
+            CollisionSystem.EnemyHit += OnEnemyHit;
+            GameStateSystem.NewGameEvent += Reset;
+        }
+
+        private void Reset() {
+            Data.points = 0;
+        }
+
+        private void OnEnemyHit(ICollider enemy, ICollider ammo) {
+            Data.points++;
+        }
 
     }
 }
